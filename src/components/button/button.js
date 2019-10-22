b63dcfa6055e80dd2a7e7063330625d5e6a1248a
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2019-10-22 09:51:09 +0100

    created components

---
+++ src/components/button/button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./button.css";
+
+const Button = props => {
+  return (<button className="btn btn-lg btn-outline-green">some text</button>)
+};
+
+export default Button;
